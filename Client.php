3837e5e6f1432abfb8081cda607058cd98c43389
Sergey Krivobokov <serega170584@gmail.com>
2022-08-17 07:50:06 +0300

    TEAM1-1041 фикс параметров загрузки

--- Client.php
+++ Client.php
@@ -1654,7 +1654,7 @@ class Client
             [
                 'name' => self::DOCUMENT_UPLOAD_FIELD_NAME,
                 'contents' => $contents,
-                'filename' => '123'
+                'filename' => null
             ]
         ];
         return $this->execMultipart('POST', 'document-uploader', $params);
